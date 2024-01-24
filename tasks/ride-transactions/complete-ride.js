c615a16132eea235e756868f41c8e467ac9f6ae0
danmesfin <danielmsfn@gmail.com>
2024-01-24 22:25:48 +0300

    updated complete ride

--- tasks/ride-transactions/complete-ride.js
+++ tasks/ride-transactions/complete-ride.js
@@ -1,11 +1,14 @@
 task("complete-ride", "Completes a ride")
   .addParam("contract", "The address of the RideTransactions contract")
-  .addParam("rideId", "The ID of the ride")
+  .addParam("rideid", "The ID of the ride")
   .addParam("distance", "The total distance traveled during the ride")
   .setAction(async (taskArgs, hre) => {
-    const rideTransactions = await hre.ethers.getContractAt("RideTransactions", taskArgs.contract);
-    await rideTransactions.completeRide(taskArgs.rideId, taskArgs.distance);
-    console.log(`Ride completed with ID: ${taskArgs.rideId}`);
+    const contractAddr = taskArgs.contract;
+    const rideId = taskArgs.rideid;
+    const distance = taskArgs.distance;
+    const rideTransactions = await hre.ethers.getContractAt("RideTransactions", contractAddr);
+    await rideTransactions.completeRide(rideId, distance);
+    console.log(`Ride completed with ID: ${rideId}`);
   });
 
 module.exports = {}
