a090290407abdc0b4a574b1467be196c80b306c9
danmesfin <danielmsfn@gmail.com>
2024-01-29 09:26:36 +0300

    fix: updated start-ride

--- tasks/ride-transactions/start-ride.js
+++ tasks/ride-transactions/start-ride.js
@@ -1,14 +1,19 @@
-task("start-ride", "Starts a new ride")
+// Task to start a ride
+task("start-ride", "Starts a ride")
   .addParam("contract", "The address of the RideTransactions contract")
-  .addParam("user", "The address of the user")
-  .addParam("estimatedendtime", "The estimated end time of the ride")
+  .addParam("user", "The address of the user requesting the ride")
+  .addParam("pickuplatitude", "The latitude of the pickup location")
+  .addParam("pickuplongitude", "The longitude of the pickup location")
   .setAction(async (taskArgs, hre) => {
     const contractAddr = taskArgs.contract;
     const user = taskArgs.user;
-    const estimatedEndTime = taskArgs.estimatedendtime;
-    const rideTransactions = await hre.ethers.getContractAt("RideTransactions", contractAddr);
-    await rideTransactions.startRide( user, estimatedEndTime);
-    console.log(`Ride started with ID: ${'TODO'}`);
-  });
+    const pickUpLocation = { 
+      latitude: parseInt(taskArgs.pickuplatitude), 
+      longitude: parseInt(taskArgs.pickuplongitude) 
+    };
 
-module.exports = {}
+    const rideTransactions = await hre.ethers.getContractAt("RideTransactions", contractAddr);
+    await rideTransactions.startRide(user, pickUpLocation);
+    console.log("Ride started successfully");
+  });
+  module.exports = {};
