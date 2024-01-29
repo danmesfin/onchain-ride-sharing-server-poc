2d0de48010bf32562f5197118a187394ce0d2701
danmesfin <danielmsfn@gmail.com>
2024-01-29 09:35:36 +0300

    updated

--- tasks/ride-transactions/index.js
+++ tasks/ride-transactions/index.js
@@ -2,3 +2,7 @@ exports.rideDetails = require("./get-ride-details.js")
 exports.processPayment = require("./process-payment.js")
 exports.completeRide = require("./complete-ride.js")
 exports.startRide = require("./start-ride.js")
+exports.cancelRide = require("./cancel-ride.js")
+exports.requestRide = require("./request-ride.js")
+exports.setCostPerUnit = require("./set-cost-per-unit.js")
+
